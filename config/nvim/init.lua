00e321369a1fd694049d8c53f896284e9d59f30b
Joshua Hegedus <josh.hegedus@outlook.com>
2023-12-23 00:31:38 +0100

    fixed eww config

---
+++ config/nvim/init.lua
@@ -0,0 +1,12 @@
+require('utils.globals')
+require('utils.functions')
+
+require('config.yozora')
+require('config.options')
+require('config.lazy')
+
+require('lsp.config')
+require('lsp.setup')
+require('lsp.functions')
+
+require('snippets.react')
