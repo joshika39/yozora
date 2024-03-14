72cf696c58c7fff99534239825c877bb2821a6c2
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-14 11:53:44 +0100

    added dynamic update

--- shell/bash/updater.sh
+++ shell/bash/updater.sh
@@ -5,18 +5,56 @@
 # if the --download option is specified, the repository files are synced to the local directory
 # the files are the: .bashrc, .bash_aliases, .bash_functions, .bash_profile, .bash_prompt, .bash_exports
 
-REPO_DIR=~/.config/yozora/shell/bash/
+REPO_DIR=$YOZORA_PATH/
+LOCAL_DIR=~/
 
 if [ ! -d $REPO_DIR ]; then
     echo "The repository directory does not exist: $REPO_DIR"
     exit 1
 fi
 
+downlaod_files() {
+  folder=${2:-"shell/bash"}
+  files=$1
+  full_path=$REPO_DIR$folder
 
-# The local directory
-LOCAL_DIR=~/
+  if [[ -z $files ]]; then
+    echo "The files are not specified"
+    exit 1
+  fi
+
+  for file in $files; do
+    if [ -f $full_path/${file#.} ]; then
+      cp $full_path/${file#.} $LOCAL_DIR$file
+    else
+      echo "The file does not exist: $full_path/${file#.}"
+    fi
+  done
+  echo "The files have been downloaded to the local directory: $LOCAL_DIR from the repository: $full_path"
+}
+
+upload_files() {
+  folder=${2:-"shell/bash"}
+  files=$1
+  full_path=$REPO_DIR$folder
+
+  if [[ -z $files ]]; then
+    echo "The files are not specified"
+    exit 1
+  fi
+
+  for file in $files; do
+    if [ -f $LOCAL_DIR$file ]; then
+      cp $LOCAL_DIR$file $full_path/${file#.}
+    else
+      echo "The file does not exist: $LOCAL_DIR$file"
+    fi
+  done
+  echo "The files have been uploaded to the repository: $REPO_DIR"
+}
 
 FILES=".bashrc .bash_aliases .bash_functions .bash_profile .bash_prompt .bash_exports"
+HOME_FILES=".xinitrc .xprofile"
 
 UPLOAD=false
 
@@ -31,25 +69,12 @@ if [ "$1" == "--download" ]; then
 fi
 
 if [ "$UPLOAD" == "true" ]; then
-    for FILE in $FILES; do
-        if [ ! -f $LOCAL_DIR$FILE ]; then
-            echo "The file does not exist: $LOCAL_DIR$FILE"
-            continue
-        fi
-        cp $LOCAL_DIR$FILE $REPO_DIR/${FILE#.}
-    done
-    echo "The local files have been uploaded to the repository"
+  upload_files "$FILES"
+  upload_files "$HOME_FILES" "home"
 fi
 
 if [ "$DOWNLOAD" == "true" ]; then
-    for FILE in $FILES; do
-        if [ ! -f $REPO_DIR/${FILE#.} ]; then
-            echo "The file does not exist: $REPO_DIR/${FILE#.}"
-            continue
-        fi
-        cp $REPO_DIR/${FILE#.} $LOCAL_DIR$FILE
-    done
-    echo "The repository files have been downloaded to the local directory"
+  downlaod_files "$FILES"
+  downlaod_files "$HOME_FILES" "home"
 fi
 
-source ~/.bashrc
