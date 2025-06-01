8e48288633da8668344a5ca495087a0a96cd7412
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-06-01 20:54:48 +0200

    feat: added fish configs

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,51 @@
+fish_add_path $HOME/scripts
+fish_add_path /usr/local/sbin
+fish_add_path /opt/homebrew/bin
+#fish_add_path $HOME/Projects/yabai/bin
+#fish_add_path $HOME/Projects/skhd/bin
+
+if type -q starship
+    eval (starship init fish)
+end
+
+set fish_greeting
+
+fish_vi_key_bindings
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+export XDG_CONFIG_HOME="/Users/joshika39/.config/"
+
+export TERM=screen-256color
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export DOCKER_SCAN_SUGGEST="false"
+export DOTNET_CLI_TELEMETRY_OPTOUT="true"
+
+export FZF_TMUX=1
+export FZF_TMUX_OPTS="-p 80%"
+
+alias ls='eza'
+
+function ssh
+    set ps_res (ps -p (ps -p %self -o ppid= | xargs) -o comm=)
+    if [ "$ps_res" = tmux ]
+        tmux rename-window (echo "ssh:" (echo $argv | cut -d . -f 1))
+        command ssh $argv
+        tmux set-window-option automatic-rename on 1>/dev/null
+    else
+        command ssh $argv
+    end
+end
+
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
